8f2f9bf5aaa012a580b8ac4f6df271a88d741dc1
usrbinkat <usrbinkat@braincraft.io>
2020-07-17 15:33:35 +0000

    relocate info output into collector repo

---
+++ info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+info () {
+bundleDu="$(du -h /root/deploy/bundle/koffer-bundle.collector-apps.tar | awk '{print $1}' | head -n 1)"
+bundlePath="$(ls /root/deploy/bundle/koffer-bundle.collector-apps.tar | head -n 1 | sed 's/root\/deploy/tmp\/platform/')"
+cat <<EOF
+
+  Koffer Bundle Complete: ${bundleDu} ${bundlePath}
+
+  Next Steps:
+    - Move to cloudctl deployment services bastion \`/tmp\`
+    - Unpack bundle into image registry path via cmd:
+
+        \`tar xv -f /tmp/koffer-bundle.collector-apps.tar -C /root/deploy/mirror\`
+
+EOF
+}
